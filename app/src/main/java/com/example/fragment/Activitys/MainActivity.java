6474b60e9b1075e38aa72c88b8aa29f8582810c9
taliyaatia-git <taliyaatia@gmail.com>
2024-03-28 14:20:38 +0200

    Initial commit

---
+++ app/src/main/java/com/example/fragment/Activitys/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.fragment.Activitys;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.fragment.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/fragment/Fragments/FragmentOne.java
@@ -0,0 +1,96 @@
+package com.example.fragment.Fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.fragment.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link FragmentOne#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class FragmentOne extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public FragmentOne() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment FragmentOne.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static FragmentOne newInstance(String param1, String param2) {
+        FragmentOne fragment = new FragmentOne();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view =  inflater.inflate(R.layout.fragment_one, container, false);
+
+        Button button = (Button) view.findViewById(R.id.buttonOne);
+        EditText text = view.findViewById(R.id.textinput);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String str = text.getText().toString();
+                Bundle bundle = new Bundle();
+                bundle.putString("textinput",str);
+                Navigation.findNavController(view).navigate(R.id.action_fragmentOne_to_fragmentThree,bundle);
+            }
+        });
+
+        //actions for button2
+        Button button2 = (Button) view.findViewById(R.id.buttonTwo);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(view).navigate(R.id.action_fragmentOne_to_fragmentTwo);
+            }
+        });
+
+
+        return view;
+
+    }
+}
